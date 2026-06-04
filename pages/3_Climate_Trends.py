7057dcb24975aca9492b7db895c716c8cfd10976
Pragna S <pragnas0506@gmail.com>
2026-06-04 12:43:26 +0530

    Update 3_Climate_Trends.py

--- pages/3_Climate_Trends.py
+++ pages/3_Climate_Trends.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
+st.title("📈 Climate Trends")
+
+df = pd.read_parquet("data/daily_weather.parquet")
+
+numeric_cols = df.select_dtypes(include="number").columns
+
+metric = st.selectbox(
+    "Select Weather Metric",
+    numeric_cols
+)
+
+fig = px.line(
+    df,
+    y=metric,
+    title=f"{metric} Trend"
+)
+
+st.plotly_chart(fig, use_container_width=True)
